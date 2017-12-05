01d0b6cf0b9ef0b14f41a0a3bd9d3bdaa58295c1
Jonas Grawe <Shemnei@users.noreply.github.com>
2017-12-05 17:07:16 +0100

    Day 5 little cleanup

--- src/d5/jumps.py
+++ src/d5/jumps.py
@@ -1,12 +1,11 @@
 def jump_one(jumps: list) -> int:
     step_count = 0
     current_index = 0
-    offset_dict = {key: 0 for key in range(0, len(jumps))}
-
-    while current_index < len(jumps):
-        offset = jumps[current_index] + offset_dict[current_index]
-        offset_dict[current_index] += 1
-        current_index = current_index + offset
+    work_list = list(jumps)
+    while current_index < len(work_list):
+        offset = work_list[current_index]
+        work_list[current_index] += 1
+        current_index += offset
         step_count += 1
     return step_count
 
@@ -14,16 +13,11 @@ def jump_one(jumps: list) -> int:
 def jump_two(jumps: list) -> int:
     step_count = 0
     current_index = 0
-    offset_dict = {key: 0 for key in range(0, len(jumps))}
-
-    while current_index < len(jumps):
-        offset_add_amount = offset_dict[current_index]
-        offset = jumps[current_index] + offset_add_amount
-        if offset >= 3:
-            offset_dict[current_index] -= 1
-        else:
-            offset_dict[current_index] += 1
-        current_index = current_index + offset
+    work_list = list(jumps)
+    while current_index < len(work_list):
+        offset = work_list[current_index]
+        work_list[current_index] += -1 if offset >= 3 else 1
+        current_index += offset
         step_count += 1
     return step_count
 
@@ -42,5 +36,5 @@ if __name__ == '__main__':
             in_lines = f.read().splitlines()
     in_lines = [int(x) for x in in_lines]
 
-    print("Jumps One: ", jump_one(in_lines))
-    print("Jumps Two: ", jump_two(in_lines))
+    print("Jumps One: ", jump_one(in_lines))  #   326618
+    print("Jumps Two: ", jump_two(in_lines))  # 21841249
